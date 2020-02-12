9b8035b58739b0716181b181c416a075b2a45731
Liang <392425595@qq.com>
2020-02-12 13:13:39 +0800

    将fake的微信支付配置提取到应用配置文件

--- src/main/java/com/imooc/pay/config/BestPayConfig.java
+++ src/main/java/com/imooc/pay/config/BestPayConfig.java
@@ -3,20 +3,24 @@ package com.imooc.pay.config;
 import com.lly835.bestpay.config.WxPayConfig;
 import com.lly835.bestpay.service.BestPayService;
 import com.lly835.bestpay.service.impl.BestPayServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BestPayConfig {
 
+    @Autowired
+    private WxAccountConfig wxAccountConfig;
+
     @Bean
     public BestPayService bestPayService() {
         //微信支付配置
         WxPayConfig wxPayConfig = new WxPayConfig();
-        wxPayConfig.setAppId("wxd898fcb01713c658");
-        wxPayConfig.setMchId("1483469312");
-        wxPayConfig.setMchKey("098F6BCD4621D373CADE4E832627B4F6");
-        wxPayConfig.setNotifyUrl("https://db09ece4.ngrok.io/pay/notify");
+        wxPayConfig.setAppId(wxAccountConfig.getAppId());
+        wxPayConfig.setMchId(wxAccountConfig.getMchId());
+        wxPayConfig.setMchKey(wxAccountConfig.getMchKey());
+        wxPayConfig.setNotifyUrl(wxAccountConfig.getNotifyUrl());
 
         BestPayServiceImpl bestPayService = new BestPayServiceImpl();
         bestPayService.setWxPayConfig(wxPayConfig);
